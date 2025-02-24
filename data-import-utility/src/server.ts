63bd9e68e0df73c999982748b41f88ec9fe7cbd1
Pushkar <pushkarv0403@gmail.com>
2025-02-25 00:15:18 +0530

    Updated folder structure and ignored sample_schema.xlsx

--- data-import-utility/src/server.ts
+++ data-import-utility/src/server.ts
@@ -1,7 +1,12 @@
 import 'dotenv/config';
+import express from 'express';
+import path from 'path';
 import app from './app';
 
 const port: number = Number(process.env.PORT) || 3000;
+
+// Serve static files from 'public'
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
