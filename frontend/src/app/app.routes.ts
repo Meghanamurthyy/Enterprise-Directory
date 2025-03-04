01b5ebe14c13ffd0f63bfad9fbd3fc9d8d208aca
anjalimelinalobo <anjalimelinalobo@gmail.com>
2025-03-04 13:31:03 +0530

    serviceforserchbyid

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,11 @@ import { EmployeeListComponent } from './components/employee-list/employee-list.
 import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
 import { AddEmployeeProgramComponent } from './components/add-employee-program/add-employee-program.component';
 import { ModifyEmployeeComponent } from './components/modify-employee/modify-employee.component';
-import { EmployeeSearchComponent } from './components/employee-search/employee-search.component'; // Import your search component
+ // Import your search component
 
 export const routes: Routes = [
   { path: 'employees', component: EmployeeListComponent },
   { path: 'employees/:id', component: EmployeeDetailsComponent },
-  { path: 'employee-search/:id', component: EmployeeSearchComponent },
   { path: 'add-employee-program', component: AddEmployeeProgramComponent },
   { path: 'modify-employee/:id', component: ModifyEmployeeComponent },
   { path: '', redirectTo: '/employees', pathMatch: 'full' },
